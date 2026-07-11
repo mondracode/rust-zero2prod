1e6ab669657bfd949a657c7fd09a230694a43b47
Santiago Mondragón Gómez <mondracode.dev@gmail.com>
2026-07-10 21:12:18 -0500

    feat: Complete health check and integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
+
 use actix_web::dev::Server;
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -10,14 +12,14 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new( || {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
